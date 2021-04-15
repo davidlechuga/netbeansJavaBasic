7bc639cf1642a292df6cb515c72b8be059c8c695
davidlechuga <lechugazteca@hotmail.com>
2021-04-14 21:24:25 -0500

    okk

---
+++ src/CaracteresEspeciales.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author davidlechuga
+ */
+public class CaracteresEspeciales {
+    public static void main (String args[]) {
+        var nombres = " Eli y David ";
+        
+        System.out.println("Nueva linea : \n" + nombres);
+        System.out.println("Tabulador : \t" + nombres);
+        System.out.println("Retroceso : \b" + nombres);
+        System.out.println("Comilla simple \'" + nombres + "\'");
+        System.out.println("Comilla doble : \"" + nombres + "\"" );
+        
+        
+    }
+}
